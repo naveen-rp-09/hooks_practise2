8778b7f36983fd900316068cc1aaab897ee14482
naveen-rp-09 <naveenjosh.2023@gmail.com>
2026-08-01 12:33:43 +0530

    Added API exercise

---
+++ api.py
@@ -0,0 +1,45 @@
+import frappe
+
+def custom_logic(doc, method):
+
+    frappe.msgprint("Hook executed!")
+
+@frappe.whitelist()
+def purchase_order_api():
+
+    purchase_order = frappe.qb.DocType("Purchase Order")
+    supplier = frappe.qb.DocType("Supplier")
+
+    query = (
+        frappe.qb
+        .from_(purchase_order)
+        .inner_join(supplier)
+        .on(purchase_order.supplier == supplier.name)
+        .select(
+            purchase_order.name,
+            purchase_order.purchase_date,
+            purchase_order.total_amount,
+            purchase_order.status,
+            supplier.supplier_name
+        )
+         .where(purchase_order.docstatus == 0)
+    )
+
+    results = query.run(as_dict=True)
+
+    # Document API
+    if results:
+        doc = frappe.get_doc("Purchase Order", results[0]["name"])
+        doc.remarks = "Updated using Document API"
+        doc.save()
+
+    # Database API
+    for row in results:
+        frappe.db.set_value(
+            "Purchase Order",
+            row["name"],
+            "remarks",
+            "Bulk Updated using Database API"
+        )
+
+    return results
