ac825aa9b19f54e9770a99499b284729742b596d
naveen-rp-09 <naveenrp0905@gmail.com>
2026-08-01 12:39:14 +0530

    Refactor purchase_order_api function
    Refactor purchase_order_api to update remarks using Document API.

--- api.py
+++ api.py
@@ -1,15 +1,11 @@
 import frappe
-
 def custom_logic(doc, method):
-
     frappe.msgprint("Hook executed!")
-
 @frappe.whitelist()
 def purchase_order_api():
 
     purchase_order = frappe.qb.DocType("Purchase Order")
     supplier = frappe.qb.DocType("Supplier")
-
     query = (
         frappe.qb
         .from_(purchase_order)
@@ -24,16 +20,13 @@ def purchase_order_api():
         )
          .where(purchase_order.docstatus == 0)
     )
-
     results = query.run(as_dict=True)
-
-    # Document API
+    
     if results:
         doc = frappe.get_doc("Purchase Order", results[0]["name"])
         doc.remarks = "Updated using Document API"
         doc.save()
 
-    # Database API
     for row in results:
         frappe.db.set_value(
             "Purchase Order",
