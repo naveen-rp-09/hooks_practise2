b22e5b19e9b5ccc362ed437fa9f5ef8970eb1623
naveen-rp-09 <naveenrp0905@gmail.com>
2026-08-17 18:22:40 +0530

    Refactor API: Replace purchase_order_api with get_recent_todos
    Removed purchase_order_api function and added get_recent_todos function to fetch recent todos with user email.

--- api.py
+++ api.py
@@ -1,38 +1,27 @@
 import frappe
-def custom_logic(doc, method):
-    frappe.msgprint("Hook executed!")
+from frappe.utils import now
 @frappe.whitelist()
-def purchase_order_api():
-
-    purchase_order = frappe.qb.DocType("Purchase Order")
-    supplier = frappe.qb.DocType("Supplier")
-    query = (
-        frappe.qb
-        .from_(purchase_order)
-        .inner_join(supplier)
-        .on(purchase_order.supplier == supplier.name)
-        .select(
-            purchase_order.name,
-            purchase_order.purchase_date,
-            purchase_order.total_amount,
-            purchase_order.status,
-            supplier.supplier_name
-        )
-         .where(purchase_order.docstatus == 0)
+def get_recent_todos():
+    todos = frappe.get_list(
+        "ToDo",
+        fields=["name", "description", "owner"],
+        order_by="creation desc",
+        limit_page_length=5
     )
-    results = query.run(as_dict=True)
-    
-    if results:
-        doc = frappe.get_doc("Purchase Order", results[0]["name"])
-        doc.remarks = "Updated using Document API"
-        doc.save()
-
-    for row in results:
-        frappe.db.set_value(
-            "Purchase Order",
-            row["name"],
-            "remarks",
-            "Bulk Updated using Database API"
+    records = []
+    for todo in todos:
+        email = frappe.db.get_value(
+            "User",
+            todo.owner,
+            "email"
         )
-
-    return results
+        records.append({
+            "name": todo.name,
+            "description": todo.description,
+            "owner": todo.owner,
+            "email": email
+        })
+    return {
+        "timestamp": now(),
+        "records": records
+    }
